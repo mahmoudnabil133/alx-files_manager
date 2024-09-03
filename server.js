68732ce2f56f02e8a14dd0380e916c53d28b81b3
mahmoudnabil133 <hodanabil144@gmail.com>
2024-09-03 19:17:46 +0300

    update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = require('./routes/index');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/', router);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
